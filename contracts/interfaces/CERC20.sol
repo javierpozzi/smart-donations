d0dabd6f6aae147bc0ed48b05733c801f6a5532a
Javier Pozzi <javierpozzi@gmail.com>
2022-01-22 08:19:41 -0300

    Refactor CERC20 interface

--- contracts/interfaces/CERC20.sol
+++ contracts/interfaces/CERC20.sol
@@ -4,25 +4,13 @@ pragma solidity ^0.8.0;
 interface CERC20 {
     function mint(uint256) external returns (uint256);
 
-    function redeem(uint256) external returns (uint256);
-
     function balanceOf(address) external view returns (uint256);
 
     function exchangeRateStored() external view returns (uint256);
 
     function exchangeRateCurrent() external returns (uint256);
 
-    function supplyRatePerBlock() external returns (uint256);
-
     function redeemUnderlying(uint256) external returns (uint256);
 
-    function balanceOfUnderlying(address owner) external returns (uint256);
-
-    function accrueInterest() external returns (uint);
-
-    function getCash() external view returns (uint);
-    function totalBorrows() external view returns (uint);
-    function totalReserves() external view returns (uint);
-    function totalSupply() external view returns (uint);
-
+    function accrueInterest() external returns (uint256);
 }
